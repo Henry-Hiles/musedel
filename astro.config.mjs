387fd3c678a587b04a3c90fdae1f936c4a667bb3
Henry-Hiles <henry@henryhiles.com>
2026-01-01 01:19:12 -0500

    change site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig, passthroughImageService } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
+    trailingSlash: "never",
+    build: { format: "directory" },
     image: { service: passthroughImageService() },
+    site: "https://henry-hiles.github.io/musedel",
 })
